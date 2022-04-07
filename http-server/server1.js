e336b8fbcd531545812539c8273df80bdc2adad6
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-04-07 19:45:33 +0900

    fs로 html 읽어 제공하기

--- http-server/server1.js
+++ http-server/server1.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const server = http
 	.createServer((req, res) => {
+		res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' }); //사파리 같은 브루우저가 html인지 문자열인지 구별못하기 때문에 이렇게 해줘야함, utf-8로 인코딩하면 한글이 깨지지 않음
 		res.write('<h1>Hello world</h1>');
 		res.write('<p>Hello server</p>');
 		res.end();
