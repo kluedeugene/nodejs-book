3e0cba765f08553ec4a55fbc00d93ddb9e972e07
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-04-13 02:39:18 +0900

    미들웨어 사용하기

--- express-server/app.js
+++ express-server/app.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+app.use((req, res, next) => {
+	console.log('모든 요청에 실행합니다.');
+	next();
+});
+
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
